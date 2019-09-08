4622de47616e90792865175778b8727b44fc589c
Shirley Wang <shirleywang527@gmail.com>
2019-09-08 20:54:30 +0200

    modify some text style and add a parallex effect

--- assets/javascript/aboutAnim.js
+++ assets/javascript/aboutAnim.js
@@ -11,3 +11,38 @@ function aboutAnim() {
 }
 
 window.addEventListener('scroll', aboutAnim)
+
+// small image parallax effect
+const aboutImg = document.querySelector('figure')
+
+function parallaxAboutImg() {
+    const multiplier = 0.2
+
+    if (isElementInViewport(aboutImg)) {
+        var distance = distanceFromLeft(aboutImg)
+        aboutImg.style.transform = `translateX(${distance*multiplier}px)`;
+    }
+}
+
+function isElementInViewport (el) {
+    var rect = el.getBoundingClientRect();
+
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+if (window.addEventListener) {
+    addEventListener('DOMContentLoaded', parallaxAboutImg, false);
+    addEventListener('load', parallaxAboutImg, false);
+    addEventListener('scroll', parallaxAboutImg, false);
+}
+
+function distanceFromLeft(el) {
+    const rect = el.getBoundingClientRect()
+    return window.innerWidth - rect.left
+    // console.log(window.innerWidth - rect.left);
+}
