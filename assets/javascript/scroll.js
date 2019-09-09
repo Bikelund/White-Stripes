fb72791e88cda95ecb4c5d1a2a982b2782028124
Bikelund <nicklas_barklund@hotmail.com>
2019-09-09 15:26:16 +0200

    General cleanup and fixes

--- assets/javascript/scroll.js
+++ assets/javascript/scroll.js
@@ -5,14 +5,13 @@ windowWidth = (windowWidth / 10) * 4;
 
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
-  if (window.pageXOffset < windowWidth) {
+  if (window.pageXOffset < windowWidth || window.pageXOffset > 15300) {
     navBar[0].style.transition = "all 1s";
     navBar[0].style.left = "-8vw";
     navBar[0].style.opacity = "0";
-} 
-else {
+  } else {
     navBar[0].style.left = "0";
     navBar[0].style.opacity = "1";
-    navBar[0].style.zIndex = "100";
+    navBar[0].style.zIndex = "1000";
   }
 }
